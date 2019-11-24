9630167450314aafef3bf2c5f9e7746316b47a9d
William Lopera <lopera.william@gmail.com>
2019-11-24 18:01:12 -0500

    Ajuste por uso CrossOrigin

--- src/main/java/com/wlopera/world/controller/SoccerWolrdCupController.java
+++ src/main/java/com/wlopera/world/controller/SoccerWolrdCupController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("${path.api}")
+@CrossOrigin(origins = "*")
 public class SoccerWolrdCupController {
 
 	@Autowired
